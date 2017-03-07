b8a5d968cefb96d3c4410f07bd08c0d5ffdb2e53
Richard <r.ash8347@gmail.com>
2017-03-07 12:35:07 -0800

    added week 3 solution code

---
+++ week-03/homework/debugging-homework/solution-code/Groceries/Groceries/GroceryColorViewController.swift
@@ -0,0 +1,58 @@
+//
+//  GroceryColorViewController.swift
+//  Groceries
+//
+//  Created by Rudd Taylor on 8/14/16.
+//  Copyright © 2016 General Assembly. All rights reserved.
+//
+
+import UIKit
+
+class GroceryColorViewController: UIViewController {
+    let grocery: Grocery
+    init(grocery: Grocery) {
+        self.grocery = grocery
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        self.navigationItem.title = "Grocery Color"
+
+        if self.grocery.title == "Banana" {
+            self.view.backgroundColor = UIColor.yellow
+        }
+        if self.grocery.title == "Orange" {
+            self.view.backgroundColor = UIColor.orange
+        }
+        //BUG: The below block does not work as it overrides the backgroundColor previously specified by 'Banana' and 'Orange' groceries.
+//        if self.grocery.title == "Apple" {
+//            self.view.backgroundColor = UIColor.greenColor()
+//        } else { // Red Apple
+//            self.view.backgroundColor = UIColor.redColor()
+//        }
+        // Fix:
+        if self.grocery.title == "Apple" {
+            self.view.backgroundColor = UIColor.green
+        }
+        if self.grocery.title == "Red Apple" {
+            self.view.backgroundColor = UIColor.red
+        }
+        // End fix
+        if self.grocery.title == "Milk" {
+            self.view.backgroundColor = UIColor.white
+        }
+        if self.grocery.title == "Cereal" {
+            self.view.backgroundColor = UIColor(red: 255/255.0, green: 239/255.0, blue: 213/255.0, alpha: 1)
+        }
+        if self.grocery.title == "Bread" {
+            self.view.backgroundColor = UIColor(red: 139/255.0, green: 119/255.0, blue: 101/255.0, alpha: 1)
+        }
+        if self.grocery.title == "Cheese" {
+            self.view.backgroundColor = UIColor(red: 255/255.0, green: 212/255.0, blue: 0/255.0, alpha: 1)
+        }
+    }
+}
