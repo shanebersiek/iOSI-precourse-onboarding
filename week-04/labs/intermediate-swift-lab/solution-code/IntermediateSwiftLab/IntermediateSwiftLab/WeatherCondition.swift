22cc542b06b389da3f180aa3b76435585b894c1b
Richard <r.ash8347@gmail.com>
2017-03-13 10:49:34 -0700

    updated solution code

---
+++ week-04/labs/intermediate-swift-lab/solution-code/IntermediateSwiftLab/IntermediateSwiftLab/WeatherCondition.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherCondition.swift
+//  IntermediateSwiftLab
+//
+//  Created by Wellington Moreno on 7/12/16.
+//  Copyright © 2016 General Assembly. All rights reserved.
+//
+
+import Foundation
+
+enum WeatherCondition : String {
+    case Sunny
+    case Cloudy
+    case Rainy
+    case Overcast
+}
---
+++ week-04/labs/intermediate-swift-lab/solution-code/IntermediateSwiftLab/IntermediateSwiftLab/WeatherViewController.swift
@@ -0,0 +1,65 @@
+//
+//  WeatherViewController.swift
+//  IntermediateSwiftLab
+//
+//  Created by Wellington Moreno on 7/12/16.
+//  Copyright © 2016 General Assembly. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var weatherConditionsLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    private let weatherImages: [WeatherCondition : UIImage] = [
+        .Sunny : #imageLiteral(resourceName: "Weather-Sunny"),
+        .Rainy : #imageLiteral(resourceName: "Weather-Rainy"),
+        .Overcast : #imageLiteral(resourceName: "Weather-Overcast"),
+        .Cloudy : #imageLiteral(resourceName: "Weather-Cloudy")
+        
+    ]
+    
+    private let blueSkiesColor = UIColor(colorLiteralRed: 63/255, green: 169/255, blue: 245/255, alpha: 1.0)
+    private let overcastColor = UIColor(colorLiteralRed: 179/255, green: 179/255, blue: 179/255, alpha: 1.0)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        WeatherService.instance.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+//MARK: Weather Updates
+extension WeatherViewController : WeatherDelegate {
+    
+    func weatherDidUpdate(weatherStatus: WeatherStatus) {
+        
+        if let image = weatherImages[weatherStatus.conditions] {
+            self.weatherImageView.image = image
+        }
+
+        self.weatherConditionsLabel.text = weatherStatus.conditions.rawValue
+        self.temperatureLabel.text = "\(weatherStatus.temperature)"
+        
+        
+        switch weatherStatus.conditions {
+            
+            case .Rainy, .Overcast :
+                self.view.backgroundColor = overcastColor
+                
+            default:
+                self.view.backgroundColor =  blueSkiesColor
+        }
+    }
+}
+
